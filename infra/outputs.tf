c51ca1135709794092173d4002b83ae0c4a8462b
prashantt597 <prashanttakkalaki1@gmail.com>
2025-08-01 00:39:58 +0530

    modified:   infra/outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,16 +1,11 @@
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "alb_url" {
-  value = "kubectl get ingress -n default -o jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}'"
-}
 # Output for EKS cluster endpoint
 output "cluster_endpoint" {
   description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
+}
+
+# Output for EKS cluster name
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
